f53587c4bc38b6910abe6aa4a159efb257375a82
Karan <karanjy7@gmail.com>
2023-10-23 00:34:49 +0530

    Update Main.java
    Again new changes made

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
     public class Main {
         public static void main(String[] args) {
             System.out.println("Hello world! This is my first O/P with IDE changes");
+            System.out.println("again new changes");
             Scanner scanner = new Scanner(System.in);
             String K = scanner.next();
             System.out.println("My input :" + K);
