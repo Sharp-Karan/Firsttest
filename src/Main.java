f0662aac38ff15530e466a21df7ad34c440d8470
Karan <126516963+Sharp-Karan@users.noreply.github.com>
2023-10-24 16:21:34 +0530

    Update Main.java
    String name changed to B from A

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
         public static void main(String[] args) {
             System.out.println("Hello world! This is my first O/P");
             Scanner scanner = new Scanner(System.in);
-            String A = scanner.next();
-            System.out.println("My input :" + A);
+            String B = scanner.next();
+            System.out.println("My input :" + B);
         }
 }
